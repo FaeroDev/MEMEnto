50bc48f6b7bc899751c4f61344d8b7dfe95625dd
Frank McDermott <fmcdermott4@gmail.com>
2021-05-20 21:17:25 -0400

    seeds for pictures added

--- seeds/pictureData.js
+++ seeds/pictureData.js
@@ -12,6 +12,22 @@ const pictureData = [
     {
         url: "https://imgflip.com/s/meme/Change-My-Mind.jpg",
         description: "Change my mind"
+    },
+    {
+        url: "https://i.guim.co.uk/img/media/44856f0611a6ae5e8f4e1875772d8f462a7e6f10/0_139_1118_671/master/1118.jpg?width=700&quality=85&auto=format&fit=max&s=3be77831a235ffc09d12cf9d18be5f25",
+        description: "Is this a pigeon"
+    },
+    {
+        url:"https://imgflip.com/s/meme/One-Does-Not-Simply.jpg",
+        description:"One does not simply"
+    },
+    {
+        url:"https://i.kym-cdn.com/entries/icons/original/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.png",
+        description:"Surprised Pikachu"
+    },
+    {
+        url:"https://imgflip.com/s/meme/Unsettled-Tom.jpg",
+        description:"Unsettled Tom"
     }
 ];
 
