de767dfb431d072bc07f19b2c4ca2c148f766aff
Matthew Pawlak <pharaohnof@gmail.com>
2021-05-26 01:52:01 -0400

    merged

--- public/js/button.js
+++ public/js/button.js
@@ -3,7 +3,6 @@ let upperTextInput = document.getElementById("upperTextInput");
 let lowerTextInput = document.getElementById("lowerTextInput");
 const subBtn = $("#submit-button");
 
-<<<<<<< HEAD
 $(phraseBtn).on("click", function fillField() {
   console.log("fillField fired" + this.dataset.uppertext);
 
@@ -12,19 +11,3 @@ $(phraseBtn).on("click", function fillField() {
 
   subBtn.click();
 });
-=======
-for (let phrase of { phrase }) {
-  makeHistoryBtn();
-}
-
-// histArr.forEach(makeHistoryBtn);
-function makeHistoryBtn(item, index) {
-  console.log("makeHistoryBtn fired");
-  const historyBtn = document.createElement("button");
-  historyBtn.setAttribute("id", `button-{phrase.id}`);
-  historyBtn.setAttribute("type", "button");
-  historyBtn.setAttribute("class", "btn border histBtn");
-  //   historyBtn.innerHTML = searchCap + ", " + item.map;
-  histBtnGrp.appendChild(historyBtn);
-}
->>>>>>> 3599fd3ac9d03c9a00e77f072142316fff382303
